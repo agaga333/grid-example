cff9a6b2d51c56c7d1a4003fccb58b4aa23d719b
agaga333 <ajankowska0404@gmail.com>
2016-10-19 22:58:37 +0200

    ewqfqew

--- task.js
+++ task.js
@@ -1,3 +1,32 @@
 /**
  * Created by Agnieszka on 2016-10-19.
  */
+var names = ['John','Mike','Jessica','Jane','Adam','Luke','Thomas','Brian','Julia','Jenifer'];
+//1//
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i] + " " + [i]);
+}
+
+//2//
+for (var i = 0; i < names.lentgh; i++) {
+    console.log(names[i].toUpperCase());
+}
+
+//3//
+
+console.log("No fucking idea")
+
+//4//
+
+var arrayLength = names.length;
+console.log(arrayLength);
+
+//5//
+
+console.log(names.join());
+
+//6//
+
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i] + " is number " + [i]);
+}

d2c8d56e45b17a340d3bafa5dc8c81d0d7896346
agaga333 <ajankowska0404@gmail.com>
2016-10-26 16:33:09 +0200

    ewqfqew

--- zadanie6.js
+++ zadanie6.js
@@ -1,3 +1,10 @@
 /**
  * Created by Agnieszka on 2016-10-26.
  */
+var input = document.getElementById("my-input");
+var addbtn = document.getElementById("add-btn");
+var content= document.getElementById("dynamic-content");
+
+console.log(input);
+console.log(addbtn);
+console.log(content);
